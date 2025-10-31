f9f915d661bc6e2669a8aefd7cd6cba85e197995
Anurag-cse <122852810+Anuraga3s@users.noreply.github.com>
2025-10-31 22:06:19 +0530

    Update CV link in AboutSection component

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="https://drive.google.com/file/d/1wfGrB7zytV9UMXRfnN_4woz_9fR9OpDy/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
